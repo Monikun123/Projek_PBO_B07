d4d33929707bd04c919d2f5cd4538d17b208ce72
TiaraSalsabella <tiarasalsabellaayuningtyas@gmail.com>
2024-11-23 21:40:20 +0700

    delete some bug

--- View/V_Login.cs
+++ View/V_Login.cs
@@ -11,53 +11,12 @@ namespace Projek_PBO_B07
             InitializeComponent();
         }
 
-        private void MainFormLogin_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void loginbutton_Click(object sender, EventArgs e)
         {
 
 
             C_LoginAkun loginakunContext = new C_LoginAkun();
             M_User login = loginakunContext.Validate(usernameText.Text, passwordText.Text);
-
-            if (login != null)
-            {
-                MessageBox.Show("Login berhasil!", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                ClearTextBox();
-
-
-            }
-            else if (string.IsNullOrEmpty(usernameText.Text) || string.IsNullOrEmpty(passwordText.Text))
-            {
-                MessageBox.Show("Username & Password tidak boleh kosong!", "FAILED", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else
-            {
-                MessageBox.Show("Username atau Password salah. Masukkan dengan benar!", "LOGIN FAILED", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
 
         }
         public void ClearTextBox()
