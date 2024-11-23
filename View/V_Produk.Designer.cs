db81d715bc8833250d8593d515777983c7ec8a80
Kemal <kemal.naufal0@gmail.com>
2024-11-23 23:37:07 +0700

    Feat: V_Produk

---
+++ View/V_Produk.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Projek_PBO_B07.View
+{
+    partial class V_Produk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(V_Produk));
+            panel1 = new Panel();
+            LogOutButton = new Button();
+            pictureBox8 = new PictureBox();
+            ProfilButton = new Button();
+            pictureBox7 = new PictureBox();
+            RiwayatButton = new Button();
+            pictureBox6 = new PictureBox();
+            button1 = new Button();
+            panel6 = new Panel();
+            label1 = new Label();
+            pictureBox5 = new PictureBox();
+            label9 = new Label();
+            pictureBox4 = new PictureBox();
+            panel2 = new Panel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(LogOutButton);
+            panel1.Controls.Add(pictureBox8);
+            panel1.Controls.Add(ProfilButton);
+            panel1.Controls.Add(pictureBox7);
+            panel1.Controls.Add(RiwayatButton);
+            panel1.Controls.Add(pictureBox6);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(panel6);
+            panel1.Controls.Add(pictureBox5);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(pictureBox4);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(203, 489);
+            panel1.TabIndex = 0;
+            // 
+            // LogOutButton
+            // 
+            LogOutButton.BackColor = Color.Red;
+            LogOutButton.Location = new Point(51, 429);
+            LogOutButton.Name = "LogOutButton";
+            LogOutButton.Size = new Size(118, 41);
+            LogOutButton.TabIndex = 14;
+            LogOutButton.Text = "LogOut";
+            LogOutButton.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = Color.Red;
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(6, 429);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(44, 41);
+            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox8.TabIndex = 13;
+            pictureBox8.TabStop = false;
+            // 
+            // ProfilButton
+            // 
+            ProfilButton.Location = new Point(51, 274);
+            ProfilButton.Name = "ProfilButton";
+            ProfilButton.Size = new Size(118, 41);
+            ProfilButton.TabIndex = 12;
+            ProfilButton.Text = "Profil";
+            ProfilButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.White;
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(6, 274);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(44, 41);
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.TabIndex = 11;
+            pictureBox7.TabStop = false;
+            // 
+            // RiwayatButton
+            // 
+            RiwayatButton.Location = new Point(51, 213);
+            RiwayatButton.Name = "RiwayatButton";
+            RiwayatButton.Size = new Size(118, 41);
+            RiwayatButton.TabIndex = 10;
+            RiwayatButton.Text = "Riwayat";
+            RiwayatButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.White;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(6, 213);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(44, 41);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 9;
+            pictureBox6.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(247, 204, 92);
+            button1.Location = new Point(51, 151);
+            button1.Name = "button1";
+            button1.Size = new Size(118, 41);
+            button1.TabIndex = 8;
+            button1.Text = "Produk";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.FromArgb(247, 204, 92);
+            panel6.Controls.Add(label1);
+            panel6.Dock = DockStyle.Top;
+            panel6.Location = new Point(0, 0);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(203, 71);
+            panel6.TabIndex = 7;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top;
+            label1.AutoEllipsis = true;
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(233, 78, 1);
+            label1.Location = new Point(47, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Fruities";
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.FromArgb(247, 204, 92);
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(6, 151);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(44, 41);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 6;
+            pictureBox5.TabStop = false;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.White;
+            label9.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(51, 104);
+            label9.Name = "label9";
+            label9.Size = new Size(107, 25);
+            label9.TabIndex = 5;
+            label9.Text = "Dashboard";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Black;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(6, 88);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(44, 41);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 4;
+            pictureBox4.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ControlLight;
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(212, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(768, 489);
+            panel2.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 21.2686558F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 78.7313461F));
+            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel2, 1, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(983, 495);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // V_Produk
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(983, 495);
+            Controls.Add(tableLayoutPanel1);
+            Name = "V_Produk";
+            Text = "V_Produk";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Panel panel1;
+        private Button LogOutButton;
+        private PictureBox pictureBox8;
+        private Button ProfilButton;
+        private PictureBox pictureBox7;
+        private Button RiwayatButton;
+        private PictureBox pictureBox6;
+        private Button button1;
+        private Panel panel6;
+        private Label label1;
+        private PictureBox pictureBox5;
+        private Label label9;
+        private PictureBox pictureBox4;
+        private Panel panel2;
+        private TableLayoutPanel tableLayoutPanel1;
+    }
+}
