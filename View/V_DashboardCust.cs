87f729ba187768a07d6091a3338a2dbddac4bebf
squirrelys <keyshakinanti12@gmail.com>
2024-12-17 20:30:13 +0700

    feat.back

--- View/V_DashboardCust.cs
+++ View/V_DashboardCust.cs
@@ -210,7 +210,9 @@ namespace Projek_PBO_B07.View
 
         private void LogOutButton_Click(object sender, EventArgs e)
         {
-           
+            this.Hide();
+            V_Login v_login = new V_Login();
+            v_login.Show();
         }
 
         private void label10_Click(object sender, EventArgs e)
